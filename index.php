94df427daea7bc0cccd59d70f00a67c14b594279
Ralf B. Lukner MD PhD <lukner@gmail.com>
2021-07-14 21:22:27 -0500

    Update index.php
    Moved <?php  > block

--- index.php
+++ index.php
@@ -2,144 +2,146 @@
 <html lang="en">
 
 <head>
-  <base href="/">
-  <!-- Required meta tags -->
-  <title>Wappler starter page</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-  <!-- Custom CSS styles-->
-  <link rel="stylesheet" href="css/style.css">
+    <base href="/">
+    <!-- Required meta tags -->
+    <title>Wappler starter page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Custom CSS styles-->
+    <link rel="stylesheet" href="css/style.css">
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 </head>
 
 <body>
-  <nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">
-    <div class="container"> <a class="navbar-brand" href="#">Lukner Medical Clinic</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <header id="page-header" class="pb-md-5 pb-5 pb-lg-0">
-    <div class="container">
-      <div class="row">
-        <div id="header-text" class="order-2 order-sm-2 order-md-1 col-12 col-sm-12 col-md-7 col-lg-6">
-          <h1 class="display-4">Patient Visit Assist</h1>
-          <p class="lead">Make your patient visits more effective and efficient!</p>
-          <?php echo '<p>This is PHP echo ... chiming in.</p>'; ?>
-          <button type="button" class="btn btn-light">Learn More</button>
+    <?php echo '<p>This is PHP.</p>'; ?>
+    <nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">
+        <div class="container"> <a class="navbar-brand" href="#">Lukner Medical Clinic</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header id="page-header" class="pb-md-5 pb-5 pb-lg-0">
+        <div class="container">
+            <div class="row">
+                <div id="header-text" class="order-2 order-sm-2 order-md-1 col-12 col-sm-12 col-md-7 col-lg-6">
+                    <h1 class="display-4">Patient Visit Assist</h1>
+                    <p class="lead">Make your patient visits more effective and efficient!</p>
+                    <button type="button" class="btn btn-light">Learn More</button>
 
+                </div>
+                <div id="header-img" class="order-1 order-sm-1 order-md-2 col-12 col-sm-12 col-md-5 col-lg-6"> <img class="img-fluid" src="assets/patient-visit.gif" width="800" height="533"> </div>
+            </div>
         </div>
-        <div id="header-img" class="order-1 order-sm-1 order-md-2 col-12 col-sm-12 col-md-5 col-lg-6"> <img class="img-fluid" src="assets/patient-visit.gif" width="800" height="533"> </div>
-      </div>
-    </div>
-  </header>
-  <section id="features" class="bg-white">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 text-center">
-          <h3 class="section-title">Features of the Patient Visit Assist Web Application</h3>
+    </header>
+    <section id="features" class="bg-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h3 class="section-title">Features of the Patient Visit Assist Web Application</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-cloud fa-4x text-muted" aria-hidden="true"></i>
+                    <h5 class="text-info">Securely Store Information</h5>
+                    <p class="text-muted">The patient visit assist web app securely stores information in the OpenEMR database and in the LMC Dropbox system so you never have to worry about losing the information or wonder where it resides.</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-address-book fa-4x text-muted" aria-hidden="true"></i>
+                    <h5 class="text-info">Easily retrieve patient charts</h5>
+                    <p class="text-muted">Select patients scheduled to be seen today.&nbsp; You can also search for and select any patient that you need.&nbsp; Finally, switch between patients effortlessly.</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-area-chart fa-4x text-muted" aria-hidden="true"></i>
+                    <h5 class="text-info">Feature 1</h5>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-bolt fa-4x text-muted" aria-hidden="true"></i>
+                    <h5 class="text-info">Access Data Quickly</h5>
+                    <p class="text-muted">The Patient Visit Assist web application instantly pulls together all the available information for a patient visit.&nbsp;</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-cog fa-4x text-muted" aria-hidden="true"></i>
+                    <h5 class="text-info">Feature 1</h5>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-database fa-4x text-muted" aria-hidden="true"></i>
+                    <h5 class="text-info">Feature 1</h5>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-cloud fa-4x text-muted" aria-hidden="true"></i>
-          <h5 class="text-info">Securely Store Information</h5>
-          <p class="text-muted">The patient visit assist web app securely stores information in the OpenEMR database and in the LMC Dropbox system so you never have to worry about losing the information or wonder where it resides.</p>
+    </section>
+    <section id="screenshots" class="bg-light">
+        <div class="container-fluid px-lg-0 px-3">
+            <div class="row align-items-center px-lg-0 no-gutters">
+                <div class="col-12 col-lg-4 mx-auto order-lg-2 mb-3">
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud officia deserunt mollit exercitation.</p>
+                    <p>Nullam vitae scelerisque est, sed tempus metus. Donec convallis volutpat enim consequat viverra. Nam blandit est eu ipsum elementum, ac pellentesque nibh placerat. Quisque venenatis pulvinar nulla, non vestibulum urna ultrices
+                        accumsan.
+                    </p>
+                    <button type="button" class="btn btn-primary">Learn More</button>
+                </div>
+                <div class="col-12 col-lg-6 order-lg-1"> <img class="img-fluid" src="http://via.placeholder.com/1400x900"> </div>
+            </div>
+            <div class="row align-items-center px-lg-0 no-gutters">
+                <div class="col-12 col-lg-4 mx-auto order-lg-1 mb-3">
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud officia deserunt mollit exercitation.</p>
+                    <p>Nullam vitae scelerisque est, sed tempus metus. Donec convallis volutpat enim consequat viverra. Nam blandit est eu ipsum elementum, ac pellentesque nibh placerat. Quisque venenatis pulvinar nulla, non vestibulum urna ultrices
+                        accumsan.
+                    </p>
+                    <button type="button" class="btn btn-primary">Learn More</button>
+                </div>
+                <div class="col-12 col-lg-6 order-lg-2"> <img class="img-fluid" src="http://via.placeholder.com/1400x900"> </div>
+            </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-address-book fa-4x text-muted" aria-hidden="true"></i>
-          <h5 class="text-info">Easily retrieve patient charts</h5>
-          <p class="text-muted">Select patients scheduled to be seen today.&nbsp; You can also search for and select any patient that you need.&nbsp; Finally, switch between patients effortlessly.</p>
+    </section>
+    <section id="faq" class="bg-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <h2>FAQs</h2>
+                    <p class="lead">Not sure how this app works?</p>
+                    <p class="text-muted">Here are the answers to some of the most common questions we hear from our appreciated customers</p>
+                </div>
+                <div class="col-12 col-md-8">
+                    <ul class="list list-unstyled">
+                        <li>
+                            <h5 class="bold">Cras elementum efficitur volutpat?</h5>
+                            <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
+                        </li>
+                        <li>
+                            <h5 class="bold">Fusce faucibus feugiat faucibus?</h5>
+                            <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
+                        </li>
+                        <li>
+                            <h5 class="bold">Fusce at mauris elementum?</h5>
+                            <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
+                        </li>
+                        <li>
+                            <h5 class="bold">Ut vel augue ut neque lobortis semper?</h5>
+                            <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-area-chart fa-4x text-muted" aria-hidden="true"></i>
-          <h5 class="text-info">Feature 1</h5>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-bolt fa-4x text-muted" aria-hidden="true"></i>
-          <h5 class="text-info">Access Data Quickly</h5>
-          <p class="text-muted">The Patient Visit Assist web application instantly pulls together all the available information for a patient visit.&nbsp;</p>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-cog fa-4x text-muted" aria-hidden="true"></i>
-          <h5 class="text-info">Feature 1</h5>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 text-center py-4"> <i class="fa fa-database fa-4x text-muted" aria-hidden="true"></i>
-          <h5 class="text-info">Feature 1</h5>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section id="screenshots" class="bg-light">
-    <div class="container-fluid px-lg-0 px-3">
-      <div class="row align-items-center px-lg-0 no-gutters">
-        <div class="col-12 col-lg-4 mx-auto order-lg-2 mb-3">
-          <h2>Lorem Ipsum</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud officia deserunt mollit exercitation.</p>
-          <p>Nullam vitae scelerisque est, sed tempus metus. Donec convallis volutpat enim consequat viverra. Nam blandit est eu ipsum elementum, ac pellentesque nibh placerat. Quisque venenatis pulvinar nulla, non vestibulum urna ultrices accumsan.
-          </p>
-          <button type="button" class="btn btn-primary">Learn More</button>
-        </div>
-        <div class="col-12 col-lg-6 order-lg-1"> <img class="img-fluid" src="http://via.placeholder.com/1400x900"> </div>
-      </div>
-      <div class="row align-items-center px-lg-0 no-gutters">
-        <div class="col-12 col-lg-4 mx-auto order-lg-1 mb-3">
-          <h2>Lorem Ipsum</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud officia deserunt mollit exercitation.</p>
-          <p>Nullam vitae scelerisque est, sed tempus metus. Donec convallis volutpat enim consequat viverra. Nam blandit est eu ipsum elementum, ac pellentesque nibh placerat. Quisque venenatis pulvinar nulla, non vestibulum urna ultrices accumsan.
-          </p>
-          <button type="button" class="btn btn-primary">Learn More</button>
-        </div>
-        <div class="col-12 col-lg-6 order-lg-2"> <img class="img-fluid" src="http://via.placeholder.com/1400x900"> </div>
-      </div>
-    </div>
-  </section>
-  <section id="faq" class="bg-white">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-4">
-          <h2>FAQs</h2>
-          <p class="lead">Not sure how this app works?</p>
-          <p class="text-muted">Here are the answers to some of the most common questions we hear from our appreciated customers</p>
-        </div>
-        <div class="col-12 col-md-8">
-          <ul class="list list-unstyled">
-            <li>
-              <h5 class="bold">Cras elementum efficitur volutpat?</h5>
-              <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
-            </li>
-            <li>
-              <h5 class="bold">Fusce faucibus feugiat faucibus?</h5>
-              <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
-            </li>
-            <li>
-              <h5 class="bold">Fusce at mauris elementum?</h5>
-              <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
-            </li>
-            <li>
-              <h5 class="bold">Ut vel augue ut neque lobortis semper?</h5>
-              <p class="mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec fringilla felis. Vestibulum consequat quam a elementum iaculis. Proin diam erat, porta eleifend lacinia ultrices.</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </section>
-  <footer id="page-footer" class="bg-dark">
-    <div class="container"> <span class="text-muted">Copyright 2021 by Lukner Medical Clinic, PLLC</span> </div>
-  </footer>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    </section>
+    <footer id="page-footer" class="bg-dark">
+        <div class="container"> <span class="text-muted">Copyright 2021 by Lukner Medical Clinic, PLLC</span> </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 </body>
 
 </html>
